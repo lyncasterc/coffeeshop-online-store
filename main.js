c69df7684c9814d149108974b107534e5a7669e8
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-10-20 01:29:34 -0400

    add slideshow

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 // Mobile Navbar
 const sideMenu = document.querySelector('#side-menu');
 const mainBody = document.querySelector('#main');
+const slides = document.querySelectorAll('.slide');
+const slideDots = document.querySelectorAll('.dot');
+let slideIndex = 0;
+let t;
+
+
+// Mobile Side Nav
 
 // open or close menu when tapping on burger
 const openSlideMenu = () => {
@@ -15,3 +22,37 @@ const closeSlideMenu = () => {
     sideMenu.style.width = '0%'
     mainBody.style.overflow = 'scroll'
 }
+
+
+
+// Index Autoslide show
+const dotSlideShow = (n) => {
+    slides[slideIndex].classList.remove('slide-active');
+    slideDots[slideIndex].classList.remove('dot-active');
+    slideIndex = n;
+    clearInterval(t);
+    slideShow();
+}
+
+const slideShow = () => {
+
+    slides[slideIndex].classList.add('slide-active');
+    slideDots[slideIndex].classList.add('dot-active');
+
+    t = setInterval(() => {
+        slides[slideIndex].classList.remove('slide-active');
+        slideDots[slideIndex].classList.remove('dot-active');
+        slideIndex++;
+
+        if(slideIndex === slides.length){
+            slideIndex = 0;
+        }
+        
+        slides[slideIndex].classList.add('slide-active');
+        slideDots[slideIndex].classList.add('dot-active');
+
+    }, 5500);
+
+};
+
+slideShow();
